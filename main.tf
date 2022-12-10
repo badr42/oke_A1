cb6bbc61e3887fb8ae0960ebb7d7411156556bbc
badr42 <badr42@gmail.com>
2022-12-10 21:42:05 +0300

    naming convention updates

--- main.tf
+++ main.tf
@@ -10,26 +10,26 @@ data "oci_identity_availability_domains" "ADs" {
 resource "oci_core_vcn" "generated_oci_core_vcn" {
 	cidr_block = "10.0.0.0/16"
 	compartment_id = var.compartment_ocid
-	display_name = "oke-vcn-A1Cluster-dbb86c919"
+	display_name = "oke-vcn-A1Cluster"
 	dns_label = "A1Cluster"
 }
 
 resource "oci_core_internet_gateway" "generated_oci_core_internet_gateway" {
 	compartment_id = var.compartment_ocid
-	display_name = "oke-igw-A1cluster1-dbb86c919"
+	display_name = "oke-igw-A1cluster1"
 	enabled = "true"
 	vcn_id = "${oci_core_vcn.generated_oci_core_vcn.id}"
 }
 
 resource "oci_core_nat_gateway" "generated_oci_core_nat_gateway" {
 	compartment_id = var.compartment_ocid
-	display_name = "oke-ngw-A1Cluster-dbb86c919"
+	display_name = "oke-ngw-A1Cluster"
 	vcn_id = "${oci_core_vcn.generated_oci_core_vcn.id}"
 }
 
 resource "oci_core_service_gateway" "generated_oci_core_service_gateway" {
 	compartment_id = var.compartment_ocid
-	display_name = "oke-sgw-A1Cluster-dbb86c919"
+	display_name = "oke-sgw-A1Cluster"
 	services {
 		service_id = "ocid1.service.oc1.eu-frankfurt-1.aaaaaaaa7ncsqkj6lkz36dehifizupyn6qjqsmtepsegs23yyntnsy7qrvea"
 	}
@@ -38,7 +38,7 @@ resource "oci_core_service_gateway" "generated_oci_core_service_gateway" {
 
 resource "oci_core_route_table" "generated_oci_core_route_table" {
 	compartment_id = var.compartment_ocid
-	display_name = "oke-private-routetable-cluster1-dbb86c919"
+	display_name = "oke-private-routetable-cluster1"
 	route_rules {
 		description = "traffic to the internet"
 		destination = "0.0.0.0/0"
@@ -57,7 +57,7 @@ resource "oci_core_route_table" "generated_oci_core_route_table" {
 resource "oci_core_subnet" "service_lb_subnet" {
 	cidr_block = "10.0.20.0/24"
 	compartment_id = var.compartment_ocid
-	display_name = "oke-svclbsubnet-A1Cluster-dbb86c919-regional"
+	display_name = "oke-svclbsubnet-A1Cluster-regional"
 	dns_label = "lbsub2f0738735"
 	prohibit_public_ip_on_vnic = "false"
 	route_table_id = "${oci_core_default_route_table.generated_oci_core_default_route_table.id}"
@@ -68,7 +68,7 @@ resource "oci_core_subnet" "service_lb_subnet" {
 resource "oci_core_subnet" "node_subnet" {
 	cidr_block = "10.0.10.0/24"
 	compartment_id = var.compartment_ocid
-	display_name = "oke-nodesubnet-A1Cluster-dbb86c919-regional"
+	display_name = "oke-nodesubnet-A1Cluster-regional"
 	dns_label = "subf3f9db2b8"
 	prohibit_public_ip_on_vnic = "true"
 	route_table_id = "${oci_core_route_table.generated_oci_core_route_table.id}"
@@ -79,7 +79,7 @@ resource "oci_core_subnet" "node_subnet" {
 resource "oci_core_subnet" "kubernetes_api_endpoint_subnet" {
 	cidr_block = "10.0.0.0/28"
 	compartment_id = var.compartment_ocid
-	display_name = "oke-k8sApiEndpoint-subnet-quick-cluster1-dbb86c919-regional"
+	display_name = "oke-k8sApiEndpoint-subnet-quick-cluster1-regional"
 	dns_label = "sub122a07b87"
 	prohibit_public_ip_on_vnic = "false"
 	route_table_id = "${oci_core_default_route_table.generated_oci_core_default_route_table.id}"
@@ -88,7 +88,7 @@ resource "oci_core_subnet" "kubernetes_api_endpoint_subnet" {
 }
 
 resource "oci_core_default_route_table" "generated_oci_core_default_route_table" {
-	display_name = "oke-public-routetable-cluster1-dbb86c919"
+	display_name = "oke-public-routetable-cluster1"
 	route_rules {
 		description = "traffic to/from internet"
 		destination = "0.0.0.0/0"
@@ -100,13 +100,13 @@ resource "oci_core_default_route_table" "generated_oci_core_default_route_table"
 
 resource "oci_core_security_list" "service_lb_sec_list" {
 	compartment_id = var.compartment_ocid
-	display_name = "oke-svclbseclist-A1cluster-dbb86c919"
+	display_name = "oke-svclbseclist-A1cluster"
 	vcn_id = "${oci_core_vcn.generated_oci_core_vcn.id}"
 }
 
 resource "oci_core_security_list" "node_sec_list" {
 	compartment_id = var.compartment_ocid
-	display_name = "oke-nodeseclist-A1cluster1-dbb86c919"
+	display_name = "oke-nodeseclist-A1cluster1"
 	egress_security_rules {
 		description = "Allow pods on one worker node to communicate with pods on other worker nodes"
 		destination = "10.0.10.0/24"
@@ -197,7 +197,7 @@ resource "oci_core_security_list" "node_sec_list" {
 
 resource "oci_core_security_list" "kubernetes_api_endpoint_sec_list" {
 	compartment_id = var.compartment_ocid
-	display_name = "oke-k8sApiEndpoint-A1cluster1-dbb86c919"
+	display_name = "oke-k8sApiEndpoint-A1cluster1"
 	egress_security_rules {
 		description = "Allow Kubernetes Control Plane to communicate with OKE"
 		destination = "all-fra-services-in-oracle-services-network"
